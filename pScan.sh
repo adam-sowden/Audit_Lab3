38692b10bae3ae232529180cb3172ebd7c71f653
Mike Leardi <MJL5549@g.rit.edu>
2018-02-27 13:14:43 -0500

    cleaned up some of the code, better variable names

--- pScan.sh
+++ pScan.sh
@@ -3,21 +3,20 @@
 
 IPADD="127.0.0.1"
 
-#function for checking port status
+#function for checking port status. Will display Port <portnum> is open if open.
 function CheckPort(){
-(echo > /dev/tcp/"$IPADD"/"$PORTNUM")>& /dev/null && echo "Port "$PORTNUM" seems to be open"
+(echo > /dev/tcp/"$IPADD"/"$PORTNUM")>& /dev/null && echo "Port "$PORTNUM" is open"
 
 }
 
-
 STARTP=$(echo $1 | cut -f1 -d-)
 ENDP=$(echo $1 | cut -f2 -d-)
-declare -a P2BS=($(seq "$STARTP" "$ENDP"))
-arraylength=${#P2BS[@]}
+declare -a PortList=($(seq "$STARTP" "$ENDP"))
+PLlength=${#PortList[@]}
 
-for (( i=1; i<${arraylength}+1; i++ ));
+for (( i=1; i<${PLlength}+1; i++ ));
 do
-  PORTNUM=${P2BS[$i-1]}
+  PORTNUM=${PortList[$i-1]}
   CheckPort IPADD PORTNUM
 done
 
