ef2435323985416b48def523562d18be0a66c9c9
Mike Leardi <MJL5549@g.rit.edu>
2018-03-03 13:33:21 -0500

    added cidr range functionality and added usage message

--- pScan.sh
+++ pScan.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 #Port scanner written in BASH
 
-function displayIPUsage(){
-echo hi
+#display the usage.
+function displayUsage(){
+echo "pScan
+Usage: pScan -ip <IP Range> -p <Port Range>
+
+-ip <IP Range>
+The -ip option is for listing the range of IP addresses that you wish to conduct a portscan on. The range may be in cidr notation or an IP range with the start of the range first and the end of the range second seperated by a dash. The IP addresses must be valid for this utility to work.
+        ex.) -ip 192.168.1.0/24 
+        ex.) -ip 192.168.1.0-192.168.1.50
+
+-p <Port Range> 	
+The -p option is for listing the ports to be scanned. The ports may be in a list seperated by commas without spaces or in a range with the starting port and the ending port seperated by a dash. The ports must be an actual port (within the values of 1-65,535) and if a range is given, have the lowest port first and the highest port second.
+                ex.) -p 22-445 
+                ex.) -p 22,23,24,25"
+
 }
 
+#Display usage when the IP address option is not valid.
+function displayIPUsage(){
+echo "-ip <IP Range>
+The -ip option is for listing the range of IP addresses that you wish to conduct a portscan on. The range may be in cidr notation or an IP range with the start of the range first and the end of the range second seperated by a dash. The IP addresses must be valid for this utility to work.
+	ex.) -ip 192.168.1.0/24 
+	ex.) -ip 192.168.1.0-192.168.1.50"
+}
+
+#Display usage when the port option is not valid.
 function displayPortUsage(){
-echo "	-p
-		The -p option is for listing the ports to be scanned. The ports may be in a list seperated by commas without spaces or in a range with the starting port and the ending port seperated by a dash. The ports must be an actual port (within the values of 1-65,535) and if a range is given, have the lowest port first and the highest port second.
+echo "-p <Port Range>
+The -p option is for listing the ports to be scanned. The ports may be in a list seperated by commas without spaces or in a range with the starting port and the ending port seperated by a dash. The ports must be an actual port (within the values of 1-65,535) and if a range is given, have the lowest port first and the highest port second.
 		ex.) -p 22-445 
 		ex.) -p 22,23,24,25"
 }
@@ -59,12 +81,51 @@ PLlength=${#PortList[@]}
 
 #function to get a list of IP addresses when cider ip given as arg.
 function getIPListCidr(){
-echo "stub"
+
+#get the cider number for the IP range.
+CIDR=$(echo ${ip} | cut -f2 -d/)
+
+#from the cidr number check to see if its in the correct format and get a list based on number.
+case $CIDR in
+
+	#get the list of IPs on a /24 network.
+	24)
+	NETWORK=$(echo ${ip} | cut -f 1-3 -d.)
+	IPListT=$(echo ${NETWORK}.{0..255})
+	IPList=$(echo ${IPListT} | cut -f 2-255 -d' ')
+	;;
+
+	#get a list of IPs on a /16 network.
+	16)
+	NETWORK=$(echo ${ip} | cut -f 1-2 -d.)
+        IPListT=$(echo ${NETWORK}.{0..255}.{0..255})
+	IPList=$(echo ${IPListT} | cut -f 2-65535 -d' ')
+	;;
+
+	#get the list of IPs on a /8 network
+	8)
+	NETWORK=$(echo ${ip} | cut -f1 -d.)
+        IPList=$(echo ${NETWORK}.{0..255}.{0.255}.{0..255})
+	IPList=$(echo ${IPListT} | cut -f 2-16777215 -d' ')
+	;;
+
+	#if the cider is not /24, /16, or /8. 
+	*)
+		displayIPUsage
+                exit 1
+	;;
+esac
 }
 
 #function to get a list of IP addresses when a range is given as arg.
 function getIPListRange(){
-echo "stub"
+
+StartIP=$(echo ${ip} | cut -f1 -d-)
+EndIP=$(echo ${ip} | cut -f2 -d-)
+
+echo $StartIP
+echo $EndIP
+
 }
 
 #takes in the arguments for the program
@@ -88,7 +149,8 @@ case $1 in
 
 	#if neither then display port usage message.
 	else
-		echo "port usage message"
+		displayPortUsage
+        	exit 1
 	fi
 	shift
 	shift
@@ -101,19 +163,17 @@ case $1 in
 	#checks to see if IP addresses are in cidr
 	if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$ ]]
 	then
-		echo "good ip bruh"
-		#getIPListCidr $ip
+		getIPListCidr $ip
 
 	#Checks to see if the IP addresses are a range.
-	elif [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\-^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
+	elif [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\-[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
 	then
-        	echo "good ip bruh"
-		#getIPListRange
+		getIPListRange $ip
 	
 	#if IP address is in neither cidr or a range display IP address usage message.
 	else 
-		echo "IP usage message"
-
+		displayIPUsage
+		exit 1
 	fi
 	shift
 	shift
@@ -121,7 +181,8 @@ case $1 in
 
 	#user uses --help to get the usage message.
 	--help)
-	echo "usage message here"
+	displayUsage 
+	exit 1
 	;;
 
 	#if nothing is submitted as arg.
@@ -131,28 +192,27 @@ case $1 in
 esac
 done
 
-declare -a IPList=("192.168.1.13")
-IPlength=${#IPList[@]}
+#make an array for the list of IPs
+IFS=' ' read -r -a IPArray <<< "$IPList" 
+IPlength=${#IPArray[@]}
 
-#chacks to see if portlist is not empty
-if [[ ! -z $PortList ]]
-then
-	#for loop to cycle through all IPs
-	for ((ip=1; ip<${IPlength}+1; ip++));
+#for loop to cycle through all IPs
+for ((ip=1; ip<${IPlength}+1; ip++));
+do
+	IPADD=${IPArray[$ip-1]}
+	echo "$IPADD"
+
+	#for loop to cycle through all ports
+	for (( prt=1; prt<${PLlength}+1; prt++ ));
 	do
-		IPADD=${IPList[$ip-1]}
-		echo "$IPADD"
 
-		#for loop to cycle through all ports
-		for (( prt=1; prt<${PLlength}+1; prt++ ));
-		do
-
-  			PORTNUM=${PortList[$prt-1]}
- 	 		#echo "$PORTNUM"
-  			CheckPort IPADD PORTNUM
-			done
+  		PORTNUM=${PortList[$prt-1]}
+		if (( $PORTNUM < 1 )) || (( $PORTNUM > 65535 ))
+		then
+			displayPortUsage
+        		exit 1
+		fi
+  		CheckPort IPADD PORTNUM
 	done
-
-else
-echo "usage message"
-fi
+	echo " "
+done
