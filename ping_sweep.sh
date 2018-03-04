fe222f1a6a63dc5594d9791cc3302dfbec546e92
Adam Sowden <a.sowden10000@gmail.com>
2018-03-04 15:55:33 -0500

    Program works for ranges provided in dash format

--- ping_sweep.sh
+++ ping_sweep.sh
@@ -12,8 +12,64 @@ fi
 dash_fmt() {
     bottom_range=$(echo $address_range | cut -d"-" -f 1)
     top_range=$(echo $address_range | cut -d"-" -f 2)
-    echo $bottom_range
-    echo $top_range
+
+    bot_first=$(echo $bottom_range | cut -d"." -f 1)
+    bot_second=$(echo $bottom_range | cut -d"." -f 2)
+    bot_third=$(echo $bottom_range | cut -d"." -f 3)
+    bot_fourth=$(echo $bottom_range | cut -d"." -f 4)
+
+    top_first=$(echo $top_range | cut -d"." -f 1)
+    top_second=$(echo $top_range | cut -d"." -f 2)
+    top_third=$(echo $top_range | cut -d"." -f 3)
+    top_fourth=$(echo $top_range | cut -d"." -f 4)
+
+   while true
+   do
+       while true
+       do
+           while true
+           do
+               while true
+               do
+                   ping_cmd=`eval "ping -c 1 $bot_first.$bot_second.$bot_third.$bot_fourth | grep from"`
+                   if [ "$ping_cmd" != "" ]
+                   then
+                       echo "Host $bot_first.$bot_second.$bot_third.$bot_fourth is up"
+                   fi
+                   if [ $bot_fourth == $top_fourth ] || [ $bot_fourth == 256 ]
+                   then
+                       break
+                   else
+                       bot_fourth=$((bot_fourth+1))
+                   fi
+               done
+               if [ $bot_third == $top_third ] || [ $bot_third == 256 ]
+               then
+                   break
+               else
+                   bot_third=$((bot_third+1))
+                   bot_fourth=0
+               fi
+           done
+           if [ $bot_second == $top_second ] || [ $bot_second == 256 ]
+           then
+               break
+           else
+               bot_second=$((bot_second+1))
+               bot_third=0
+               bot_fourth=0
+           fi
+       done
+       if [ $bot_first == $top_first ] || [ $bot_first == 256 ]
+       then
+           break
+       else
+           bot_first=$((bot_first+1))
+           bot_second=0
+           bot_third=0
+           bot_fourth=0
+       fi
+   done
 
     exit 0
 }
