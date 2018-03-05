d34664182d131742efc2a98256fb02b1c208ebd6
Sola Alabi <Sola@Dunsin-Alabis-MacBook-Pro.local>
2018-03-05 12:13:28 -0500

    Implemented ping testing

--- osClass.py
+++ osClass.py
@@ -1,7 +1,8 @@
 import os
 import sys
+import platform
 
-def parseFile( file) :
+def parseFile( file ):
         file = open ( file)
         lst = []
         for line in file :
@@ -9,11 +10,21 @@ def parseFile( file) :
                 lst.append(line)
         return lst
 
+def pingTest( ipaddr ):
+    hostname = ipaddr
+    response = os.system("ping -c 1 " + hostname)
+    # and then check the response...
+    if response == 0:
+        pingstatus = True
+    else:
+        pingstatus = False
+    return pingstatus
 
 
 def main():
         lst = parseFile(sys.argv[1])
-        print(lst)
+        for item in lst:
+		print(pingTest(item))
 
 if __name__ == "__main__":
     	main()
