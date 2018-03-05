f6a86f5e33d7af3536ebcc40a6a8b010484ccf62
Sola Alabi <Sola@Dunsin-Alabis-MacBook-Pro.local>
2018-03-05 11:57:58 -0500

    implemented file parsing to get list of ip addresses

--- osClass.py
+++ osClass.py
@@ -1,5 +1,20 @@
 import os
+import sys
+
+def parseFile( file) :
+        file = open ( file)
+        lst = []
+        for line in file :
+                line = line.strip()
+                lst.append(line)
+        return lst
+
 
 
 def main():
+        lst = parseFile(sys.argv[1])
+        print(lst)
 
+if __name__ == "__main__":
+    	main()
+	main(sys.argv)
