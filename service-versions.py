d6e4f92c3f02afd2d423c87d6120097056b6e9f1
Adam Sowden <a.sowden10000@gmail.com>
2018-03-06 10:29:52 -0500

    Added timeout to prevent script hanging on certain ports that have no banner

--- service-versions.py
+++ service-versions.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
 
-import socket, sys, os
+import socket, sys, os, threading
 
 def grab_banner(ip, port):
     s = socket.socket()
-    isOpen = s.connect_ex((ip,port))
-    if not isOpen:
-        banner = s.recv(1024)
+    s.settimeout(10)
+    isClosed = s.connect_ex((ip,port))
+    if not isClosed:
+        try:
+            banner = s.recv(1024)
+        except socket.timeout, e:
+            err = e.args[0]
+            if err == "timed out":
+                banner = ""
         if banner != "":
-            print(str(port) + ": " + banner)
+            print(str(port) + ": " + banner.strip('\n'))
         else:
             print("Port " + str(port) + " is open but no service banner was found.")
     
@@ -18,8 +24,11 @@ def grab_banner(ip, port):
 def main():
     ip = sys.argv[1]
     print(ip + ":")
+    threads = []
     for port in range(0,65535):
         grab_banner(ip, port)
+    
+    sys.exit(0)
 
 
 if __name__ == '__main__':
